838a7cf150d391fd355003267217a5ce6390487e
Les Orchard <me@lmorchard.com>
2016-02-15 13:27:52 -0500

    Further mac keymap tweaks

--- converter/ps2_usb/keymap_modelmac.c
+++ converter/ps2_usb/keymap_modelmac.c
@@ -19,7 +19,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `-----------------------------------------------------------' `-----------' `---------------'
      */
     KEYMAP(
-    GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  F10, F11, F12,           VOLD,VOLU,MUTE,
+    GRV, F1,  F2,  F3,  F4,  F5,  F6,  F7,  F8,  F9,  MRWD, MPLY, MFFD,        MUTE,VOLD,VOLU,
     ESC, 1,   2,   3,   4,   5,   6,   7,   8,   9,   0,   MINS,EQL, BSPC,     INS, HOME,PGUP,    NLCK,PSLS,PAST,PMNS,
     TAB, Q,   W,   E,   R,   T,   Y,   U,   I,   O,   P,   LBRC,RBRC,BSLS,     DEL, END, PGDN,    P7,  P8,  P9,
     LCTL,A,   S,   D,   F,   G,   H,   J,   K,   L,   SCLN,QUOT,     ENT,                         P4,  P5,  P6,  PPLS,
@@ -46,7 +46,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TRNS,TRNS,TRNS,ESC, TRNS,TRNS,TRNS,HOME,UP,  END, PGUP,SLCK,PAUS,INS,      DEL, END, PGDN,    P7,  P8,  P9,
     TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,PGUP,LEFT,DOWN,RGHT,PGDN,TRNS,     TRNS,                        P4,  P5,  P6,  PPLS,
     TRNS,TRNS,TRNS,TRNS,TRNS,SPC, PGDN,GRV, FN1, TRNS,APP,           TRNS,          UP,           P1,  P2,  P3,
-    TRNS,TRNS,TRNS,          TRNS,                    RALT,TRNS,TRNS,TRNS,     LEFT,DOWN,RGHT,    P0,       PDOT,PENT
+    LCTL,TRNS,LALT,          TRNS,                    RALT,TRNS,TRNS,TRNS,     LEFT,DOWN,RGHT,    P0,       PDOT,PENT
     ),
 };
 
